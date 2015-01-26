5f3e6d0396c877d1b5665b5b80aa5c757fe90303
diwakar <diwakar.punjani@gmail.com>
2015-01-25 18:22:16 -0800

    Initial Commit

--- Finance/FriendsViewController.swift
+++ Finance/FriendsViewController.swift
@@ -6,9 +6,9 @@
 //  2. Contact
 //      a. Details - follow button
 //      b. Stocks being followed
-//  3. Conversations
-//      a. History
-//      b. Start a new one
+//      c. Conversations
+//          i. History
+//          ii. Start a new one (floating)
 //
 //  Created by diwakar punjani on 1/25/15.
 //  Copyright (c) 2015 diwakar punjani. All rights reserved.
